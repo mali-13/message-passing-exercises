aed43cc253a89bb6c80a16248cf1ab834353a38b
mali <hello@xhemaj.dev>
2024-03-27 15:23:29 +0100

    feat: Define flask get and post endpoint

--- lesson-3-implementing-message-passing/flask-starter/app.py
+++ lesson-3-implementing-message-passing/flask-starter/app.py
@@ -1,10 +1,16 @@
-import json
 from flask import Flask, jsonify, request
 
-from .services import retrieve_orders, create_order
+from services import retrieve_orders, create_order
 
 app = Flask(__name__)
 
+@app.route('/api/orders/computers', methods=['GET'])
+def get_computer_orders():
+    return retrieve_orders()
+
+@app.route('/api/orders/computers', methods=['POST'])
+def create_computer_order():
+    return create_order(request.json)
 
 @app.route('/health')
 def health():
